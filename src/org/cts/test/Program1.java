abdddbc515dc5e4ae43211b44a6cd7f885913539
Saranyatmc1996 <saranyatmc1996@gmail.com>
2019-07-01 10:20:49 +0530

    New code added

---
+++ src/org/cts/test/Program1.java
@@ -0,0 +1,18 @@
+package org.cts.test;
+
+public class Program1 {
+
+
+
+	public static void main(String[] args) {
+		int a=4; int fact = 1;
+		for(int i=1;i<=a; i++)
+		{
+			fact=fact*i;
+		}	
+			System.out.println(fact);
+		}
+
+	}
+
+
